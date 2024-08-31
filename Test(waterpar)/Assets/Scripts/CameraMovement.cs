5cc8a7ab36c4968bd64dc76fa926cc7a336b740d
Honzaakk <honzik.columbus@gmail.com>
2024-08-31 18:39:59 +0200

    CameraMovement

--- Test(waterpar)/Assets/Scripts/CameraMovement.cs
+++ Test(waterpar)/Assets/Scripts/CameraMovement.cs
@@ -23,5 +23,13 @@ public class CameraMovement : MonoBehaviour
     {
         float mousX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mousY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        RotationX -= mousY;
+        RotationY += mousX;
+
+        RotationX = Mathf.Clamp(RotationX, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(RotationX, RotationY, 0);
+        orientation.rotation = Quaternion.Euler(0, RotationY, 0);
     }
 }
