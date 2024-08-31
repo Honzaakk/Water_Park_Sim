3c003e628016c03295bf86660e0319e24b0c31de
Honzaakk <honzik.columbus@gmail.com>
2024-08-31 20:32:18 +0200

    PlayerMovement

---
+++ Test(waterpar)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed;
+
+    public Transform orientation;
+
+    float verticalInput;
+    float horizontalInput;
+    float UpInput;
+
+    public Vector3 moveDirection;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MoveInput()
+    {
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            UpInput = 1;
+            Debug.Log(" pressing");
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            UpInput = -1;
+            Debug.Log(" pressing");
+        }
+        else 
+        {
+            UpInput = 0;
+            Debug.Log("isnt pressing");
+        }
+        
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput + orientation.up * UpInput;
+
+        transform.position += moveDirection.normalized * playerSpeed * Time.deltaTime;
+    }
+}
